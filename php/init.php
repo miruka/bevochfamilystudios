e16a93946421c399de72b8995528e32d0b0ab08d
miruka <xchrismiruka@gmail.com>
2020-03-17 16:58:41 +0300

    DB Connection

---
+++ php/init.php
@@ -0,0 +1,8 @@
+<?php
+$db = mysqli_connect('localhost', 'pmauser', 'Dypers1481*', 'bevochfamilystudios');
+if (mysqli_connect_errno()) {
+    echo 'Database Connection Failed with the following errors ' . mysqli_connect_errno();
+    die();
+} else {
+    echo 'Connection Succesful';
+}
